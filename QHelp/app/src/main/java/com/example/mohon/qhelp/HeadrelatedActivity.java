78b5c0bc76b505f5c0923642e6e6cde849dd0914
maxyee <eyaminkhan00@gmail.com>
2018-12-17 16:05:55 +0600

    Added another page headrelatedactivity for the project

---
+++ QHelp/app/src/main/java/com/example/mohon/qhelp/HeadrelatedActivity.java
@@ -0,0 +1,13 @@
+package com.example.mohon.qhelp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HeadrelatedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_headrelated);
+    }
+}
