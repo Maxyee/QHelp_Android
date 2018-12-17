0e78e886d34d12e773e5aa4c1c8a8199c1e4369e
maxyee <eyaminkhan00@gmail.com>
2018-12-17 15:10:18 +0600

    Added the Problem activity page for the project

---
+++ QHelp/app/src/main/java/com/example/mohon/qhelp/ProblemsActivity.java
@@ -0,0 +1,13 @@
+package com.example.mohon.qhelp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProblemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_problems);
+    }
+}
