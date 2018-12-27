a4623c57c6bc150d6ea2c6d13950cdf332a7cfd9
maxyee <eyaminkhan00@gmail.com>
2018-12-28 03:14:23 +0600

    Working on the Google maps for this project

--- QHelp/app/src/main/java/com/example/mohon/qhelp/HomeActivity.java
+++ QHelp/app/src/main/java/com/example/mohon/qhelp/HomeActivity.java
@@ -1,13 +1,64 @@
 package com.example.mohon.qhelp;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = "HomeActivity";
+
+    private static final int ERROR_DIALOG_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        if (isServicesOK()){
+            init();
+        }
     }
+
+    private void init(){
+        Button btnMap = (Button) findViewById(R.id.button2);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public boolean isServicesOK(){
+        Log.d(TAG,"isServicesOk: checking google service version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(HomeActivity.this);
+
+        if (available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG,"isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d(TAG,"isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(HomeActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else{
+            Toast.makeText(this,"you can't make map request", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
 }
